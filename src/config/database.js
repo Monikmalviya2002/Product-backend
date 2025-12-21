8860acecc29657fe358009b94da593f73c01e714
Monik Malviya <monikmalviya04gmail.com>
2025-12-21 11:16:05 +0530

    connected DB to server

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+import mongoose  from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+  async function connectDB() {
+         try{
+           await mongoose.connect( process.env.DB_CONNECTION_STRING);
+           console.log("Database connection successfull")
+         }catch(err){
+           console.error("Database connection failed", err);
+            
+       }
+  }
+
+  export default connectDB;
