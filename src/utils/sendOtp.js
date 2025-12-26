40297847b6578528eb753db220e27b42c8b2ffe3
Monik Malviya <monikmalviya04gmail.com>
2025-12-26 11:20:10 +0530

    add nodemailer

--- src/utils/sendOtp.js
+++ src/utils/sendOtp.js
@@ -1,29 +1,26 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, 
+  },
+});
 
 const sendOtp = async (email, otp) => {
-  try {
-    await resend.emails.send({
-      from: "Productr <onboarding@resend.dev>",
-      to: email,
-      subject: "Your Productr Login OTP",
-      html: `
-        <h2>Login OTP</h2>
-        <p>Your OTP is:</p>
-        <h1>${otp}</h1>
-        <p>This OTP is valid for 5 minutes.</p>
-      `,
-    });
+  const mailOptions = {
+    from: `"Productr" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: "Your Productr Login OTP",
+    html: `<h1>${otp}</h1><p>This OTP is valid for 5 minutes.</p>`,
+  };
 
-    console.log(`✅ OTP email sent to ${email}`);
-  } catch (error) {
-    console.error("❌ Resend email error:", error);
-    throw error;
-  }
+  await transporter.sendMail(mailOptions);
+  console.log(`OTP sent to ${email}`);
 };
 
 export default sendOtp;
